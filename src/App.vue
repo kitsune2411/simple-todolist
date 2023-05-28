474fe959aa70608bab6aaa911d97df6dcfa576f2
AgungAwan <agungkristiawan24@gmail.com>
2023-05-28 18:17:30 +0800

    make it position center

--- src/App.vue
+++ src/App.vue
@@ -4,35 +4,19 @@ import TodoList from './components/TodoList.vue';
 </script>
 
 <template>
-  <TodoForm />
-  <TodoList />
+  <main>
+    <TodoForm />
+    <TodoList />
+  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+main {
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  align-content: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+@media (min-width: 1024px) {}
 </style>
